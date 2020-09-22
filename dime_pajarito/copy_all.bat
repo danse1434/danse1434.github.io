13cf967ba27bbbd7f9c6fdf42ac3ceba814bf9d3
Daniel Parra <dsparrag@unal.edu.co>
2020-09-22 16:55:09 -0500

    Modificacion|Adicion Ciencia Datos

---
+++ dime_pajarito/copy_all.bat
@@ -0,0 +1,11 @@
+ECHO OFF
+SET source_dir="C:\Users\ACER\Desktop\DS4A\"
+SET target_dir="F:\Documentos\Personal\Curriculum & Resume\WEB\danse1434.github.io\dime_pajarito"
+
+REM for /R %source_dir% %%f in (*.html *.png) do echo %%f
+
+REM xcopy C:\Users\ACER\Desktop\DS4A\*.html %target_dir% /e
+REM xcopy C:\Users\ACER\Desktop\DS4A\*.png %target_dir% /e
+
+PAUSE
+PAUSE
