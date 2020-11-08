6a5ec3fb0467f9864da4ff27e596cbc78f9698d2
Daniel Parra <kdcloop@gmail.com>
2020-11-08 18:52:43 -0500

    Modificacion de color_change a modulo

--- dime_pajarito/00_general/color_change.js
+++ dime_pajarito/00_general/color_change.js
@@ -3,31 +3,34 @@ let index = 0;
 
 
 // Functions
-function selectbg(name) {
+function selectbg(name, refData) {
     let res_a = ''; let res_b = '';
     name = name.trim().toLowerCase(); // Remove spaces and 
 
     // console.log(name);
-    for (let i = 0; i < varlist['name'].length; i++) {
-        let name_1 = varlist['name'][i].trim().toLowerCase();
+    for (let i = 0; i < refData['name'].length; i++) {
+        let name_1 = refData['name'][i].trim().toLowerCase();
         
         
 
         if (name_1 == name) {
-            res_a = varlist['bg_color'][i]
-            res_b = varlist['color'][i]
+            res_a = refData['bg_color'][i]
+            res_b = refData['color'][i]
         };
     }
     return [res_a, res_b];
 }
 
-cobl_ls = document.getElementsByTagName('cobl')
+// Ejecutar la función de lectura de readDataJSON
+//let varlist = readDataJSON("./00_general/data.json"); // Forzar su ejecución antes
+
+let cobl_ls = document.getElementsByTagName('cobl');
 
 for (let i = 0; i < cobl_ls.length; i++) {
-    valTuple = selectbg(cobl_ls[i].innerText);
+    let valTuple = selectbg(cobl_ls[i].innerText, varlist);
     document.getElementsByTagName('cobl')[i].style['background'] = valTuple[0];
     document.getElementsByTagName('cobl')[i].style['color'] = valTuple[1];
 
     // console.log( cobl_ls[i].innerText );
-    // console.log( selectbg(cobl_ls[i].innerText) );
+    // console.log( selectbg(cobl_ls[i].innerText, varlist) );
 }
