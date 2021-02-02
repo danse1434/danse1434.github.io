2c6115594abe5f4da89f1553dfae1d230901b35b
Daniel Parra <kdcloop@gmail.com>
2021-02-02 15:57:17 -0500

    Estilos de imagen

--- Modulos/Blog/06_Markov_Decission_Process/src/diagrama.js
+++ Modulos/Blog/06_Markov_Decission_Process/src/diagrama.js
@@ -1,5 +1,5 @@
-import {multiplyMatrixAndPoint, convertRange, matA, matB, initVec} from './algebraLineal.js'
-import {generarMatriz} from './generarMatriz.js'
+import {multiplyMatrixAndPoint, convertRange, matA, matB, initVec} from './algebraLineal.js?20210202'
+import {generarMatriz} from './generarMatriz.js?20210202'
 
 
 var configButton =  document.getElementById("Configuracion")
@@ -11,21 +11,19 @@ output.innerHTML = `<b>Escala de tiempo</b>: ${slider.value} Años`; // Display 
 configButton.onclick = function(){
   let matEx = generarMatriz('cellMat', 'INPUT');
   // console.log(matEx);
-  simulacion(slider.value, matEx);
+  simulacion(parseInt(slider.value), matEx);
 }
 
 // Actualizar el valor de slider
 slider.oninput = function () {
   output.innerHTML = `<b>Escala de tiempo</b>: ${this.value} Años`;
   let matEx = generarMatriz('cellMat', 'INPUT');
-  simulacion(this.value, matEx);
+  simulacion(parseInt(this.value), matEx);
 };
 
-var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+var margin = { top: 10, right: 20, bottom: 50, left: 150 },
   width = 800 - margin.left - margin.right,
-  height = 600 - margin.top - margin.bottom,
-  padding = 20,
-  barPadding = 1;
+  height = 600 - margin.top - margin.bottom;
 
 var svg = d3
   .select("#graficoProcesoMarkov")
@@ -54,6 +52,37 @@ var path3 = svg.append("path")
   .attr("stroke", "red")
   .attr("stroke-width", 5);
 
+var legendText = [];
+var legendLines = [];
+
+for (let i = 0; i < 3; i++) {
+  legendLines[i] = svg.append('line');
+
+  legendText[i] = svg.append('text')
+    .attr('color', "black")
+    .attr("font-family", "sans-serif")
+    .style("font-size", "20px")
+    .attr("alignment-baseline","middle");
+}
+
+// Títulos de ejes
+svg.append('text')
+  .attr("transform", "translate(" + (width/2) + ", " + (height + margin.top + 40) + ")")
+  .style('text-anchor', 'middle')
+  .style('font-weight', 'bold')
+  .style("font-size", "20px")
+  .text('Años');
+
+svg.append('text')
+  .attr('transform', 'rotate(-90)')
+  .attr('x', 0 - (height/2))
+  .attr('y', 0 - margin.left)
+  .attr('dy', '1em')
+  .style('text-anchor', 'middle')
+  .style('font-weight', 'bold')
+  .style("font-size", "20px")
+  .text('Número de pacientes');
+
 function simulacion(timeSteps, transitionMatrix) {
   
   let matVEC = [];
@@ -66,19 +95,21 @@ function simulacion(timeSteps, transitionMatrix) {
 
   var x_scale = d3.scaleLinear()
     .domain([1, timeSteps])
-    .range([padding, width - padding])
+    .range([0, width])
     .clamp(true);
 
   var y_scale = d3.scaleLinear()
     .domain([0, 1e7])
-    .range([height - padding, padding])
+    .range([height, 0])
     .clamp(true);
     
-  xAxis.call(d3.axisLeft(y_scale))
-    .attr("transform", "translate(" + 20 + ", " + 0 + ")");
+  xAxis.call(d3.axisLeft().scale(y_scale))
+    .attr("transform", "translate(" + 0 + ", " + 0 + ")")
+    .attr('font-size', '24px');
 
-  yAxis.call(d3.axisBottom(x_scale))
-    .attr("transform", `translate( ${0}, ${height - 16} )`);
+  yAxis.call(d3.axisBottom().scale(x_scale))
+    .attr("transform", `translate( ${0}, ${height} )`)
+    .attr('font-size', '24px');
 
   path1.datum(matVEC)
     .attr("d", d3.line()
@@ -105,23 +136,27 @@ function simulacion(timeSteps, transitionMatrix) {
   ]
   for (let i = 0; i < legendObject.length; i++) {
     const element = legendObject[i];
-    // console.log(element);
 
-    svg.append('line')
+    legendLines[element['pos']]
       .attr('x1', (d,i) => {return x_scale( convertRange([0,1], [1, timeSteps+1 ], element['xmin'] ) )})
       .attr('x2', (d,i) => {return x_scale( convertRange([0,1], [1, timeSteps+1 ], element['xmax'] ) )})
       .attr('y1', (d,i) => {return y_scale( convertRange([0,1], [0, 1E7], element['y'] ))})
       .attr('y2', (d,i) => {return y_scale( convertRange([0,1], [0, 1E7], element['y'] ))})
       .attr('stroke', element['color']).attr('stroke-width', 5);
 
-    svg.append('text')
-      .attr('x', (d,i) => {return x_scale( convertRange([0,1], [1, timeSteps+1 ], element['xmax']+0.02 ) )} )
+    legendText[element['pos']]
+      .attr('x', (d,i) => {
+        console.log(
+          // element['xmax'], 
+          // timeSteps,
+          // convertRange([0,1], [1, timeSteps+1 ], element['xmax'] ) ,
+          // parseInt(timeSteps),
+          // convertRange([0,1], [1, parseInt(timeSteps)+1 ], element['xmax'] + 0.2) 
+          );
+        return x_scale( convertRange([0,1], [1, timeSteps+1 ], element['xmax'] ) )
+      })
       .attr('y', (d,i) => {return y_scale( convertRange([0,1], [0, 1E7], element['y'] ))} )
-      .text(element['text'])
-      .attr('color', "black")
-      .attr("font-family", "sans-serif")
-      .style("font-size", "16px")
-      .attr("alignment-baseline","middle");
+      .text(element['text']);
   }
 }
 
